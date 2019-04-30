cab932b487cda1075a7b16c3d9fe4d73cbbca5aa
unknown <bjornetorp@hotmail.com>
2019-04-30 16:02:20 +0200

    added event listener + imported dropbox etc

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import './App.css';
 import Home from './Home';
 import Favorite from './Favorite';
 import logoImage from './logo.png';
+import { Dropbox } from 'dropbox';
+// import Dropbox from 'dropbox/dropbox'; <-- According to https://dropbox.github.io/dropbox-sdk-js/tutorial-Getting%20started.html
+//                                        this should be used with Babel, but this creates an error because 'Dropbox' has already been declared.
+import fetch from 'isomorphic-fetch';
 
 const h3Style = {
   marginTop: "60px"
@@ -33,20 +37,24 @@ const headerStyle = {
   top: "20px"
 }
 
-const Start = () => (
+const Start = (props) => (
   <>
     <Helmet>
       <title>Start</title>
     </Helmet>
 
     <h3 style={h3Style}>File Hosting Service</h3>
-    <Button color="success">Connect</Button>{' '}
+    <Button color="success" onClick={props.onClickConnect}>Connect</Button>{' '}
   </>
 );
 
 
 
 function App() {
+  function onClickConnect(event) {
+    console.log('connecting');
+  }
+
   return (
     <div className="App">
       <Router>
@@ -54,7 +62,7 @@ function App() {
           <Link to="/"><img src={logoImage} style={logoStyle} /></Link>
           <h1 style={headerStyle}>TeaCup</h1>
         </nav>
-        <Start />
+        <Start onClickConnect={onClickConnect}/>
         <Route exact path="/" component={Home} />
         <Route path="/favorite" component={Favorite} />
       </Router>
