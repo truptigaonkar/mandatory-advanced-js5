04b4b63d3089ac5e1f47ce0c26434807e7b73583
truptigaonkar <trups11.trupti@gmail.com>
2019-05-06 18:35:33 +0200

    truptibranch: Search functionality added successfully

--- src/Home.js
+++ src/Home.js
@@ -8,64 +8,83 @@ let ACCESS_TOKEN = 'u0siLycEZIAAAAAAAAAA3NVwbhi2hLMF8YtFvS6mL9qzqE2Bb9qNuivhETRL
 
 function Home(props) {
 
- const [data, updateData] = useState([]);
- const [token, updateToken] = useState(token$.value);
+  const [data, updateData] = useState([]);
+  const [token, updateToken] = useState(token$.value);
+  const [search, updateSearch] = useState('');
 
- useEffect(() => {
-  const subscription = token$.subscribe(updateToken);
-  return () => subscription.unsubscribe();
- }, []);
+  // Using this instead of helmet because it was causing problem while search
+  useEffect(() => {
+    document.title = "Home";
+  })
 
- useEffect(() => {
+  useEffect(() => {
+    const subscription = token$.subscribe(updateToken);
+    return () => subscription.unsubscribe();
+  }, []);
 
+  useEffect(() => {
+
+    // If token exists
     if (token) {
-   let dropbox = new Dropbox({accessToken: token});
 
-      //Fetching all folders
-      dropbox.filesListFolder({path: ''})
-      .then(function(response) {
-        console.log("Response: ", response.entries);      
-        updateData(response.entries);
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-    }
-      
- }, [token]);
+      let dropbox = new Dropbox({ accessToken: token });
 
- console.log("Data: ", data);
-  return (
-    <>
-      <Helmet>
-        <title>Home</title>
-      </Helmet>
-      {
-        data.length === 0 ? <div>Loading...</div> :
-        <table border='1'>
-          <thead>
-            <tr>
-              <th>File type</th>
-              <th>File name</th>
-              <th>Last modified</th>
-              <th>Size</th>  
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((file) => {
-              return (
-                <tr key={file.id}>
-                  <td>{file[".tag"]}</td>
-                  <td>{file.name}</td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              )
-            })}
-          </tbody>
-       </table>
+      // if then else for search
+      if (!search) {
+        //Fetching all folders
+        dropbox.filesListFolder({ path: '' })
+          .then(function (response) {
+            console.log("Response: ", response.entries);
+            updateData(response.entries);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      } else {
+        // Search
+        dropbox.filesSearch({ path: '', query: search })
+          .then(function (response) {
+            const files = response.matches.map(file => {
+              return file.metadata
+            });
+
+            console.log(files)
+            updateData(files)
+          })
       }
 
+    }
+
+  }, [token, search]);
+
+  console.log("Data: ", data);
+  return (
+    <>
+
+      <input type="text" placeholder="search..." onChange={(e) => { updateSearch(e.target.value); }} value={search} />
+
+      <table border='1'>
+        <thead>
+          <tr>
+            <th>File type</th>
+            <th>File name</th>
+            <th>Last modified</th>
+            <th>Size</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((file) => {
+            return (
+              <tr key={file.id}>
+                <td>{file[".tag"]}</td>
+                <td>{file.name}</td>
+                <td></td>
+                <td></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
