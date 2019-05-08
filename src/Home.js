7b71af23590742cb6cd83ea6732ad2589e68e640
truptigaonkar <trups11.trupti@gmail.com>
2019-05-08 09:31:19 +0200

    truptibranch: Table data Last modified column is updated successfully

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import { Dropbox } from 'dropbox';
 import { updateToken } from './Store.js'
 import { token$ } from './Store.js';
 import { Redirect } from 'react-router-dom';
+import moment from 'moment';
 
 function Home(props) {
 
@@ -71,6 +72,33 @@ function Home(props) {
     return <Redirect to="/start" />;
   }
 
+  // Table data Last modified calculations
+  function handleLastModified (date) {
+    // if (moment(new Date()).diff(date, 'days') > 2) {
+    //   return moment(date).format('Do MMMM YYYY, h:mm')
+    // } else {
+    //   return moment(date).fromNow();
+    // }
+
+    let day = date.substring(8, 10);
+    let month = date.substring(5, 7);
+    let year = date.substring(0, 4);
+
+    let months = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+    
+    month = month.replace(/^0+/, '');
+    day = day.replace(/^0+/, '');
+    let monthShow = months[month-1];
+    
+
+    return <label>{ day + ' ' + monthShow + ' ' + year}</label>
+  }
+
   console.log("Data: ", data);
 
   return (
@@ -98,7 +126,7 @@ function Home(props) {
               <tr key={file.id}>
                 <td>{file[".tag"] === "folder" ? <i class="material-icons">folder_open</i> : file[".tag"]}</td>
                 <td>{file.name}</td>
-                <td></td>
+                <td>{file.server_modified ? handleLastModified(file.server_modified) : null}</td>
                 <td></td>
                 <td><i class="material-icons">more_horiz</i></td>
                 <td><i class="material-icons" onClick={onClickFavorite}>star_border</i></td>
