f9570f766f2806da4012ebe3402b908b5c95f1e6
truptigaonkar <trups11.trupti@gmail.com>
2019-05-12 17:38:34 +0200

    Component Thumbnail is added with filetypes: image, file, folder

--- src/Components/Thumbnail.js
+++ src/Components/Thumbnail.js
@@ -8,30 +8,35 @@ const Thumbnail = props => {
   const [url, updateUrl] = useState('');
 
   useEffect(() => {
-    if(fileType.test(props.file.name)){
+    if (fileType.test(props.file.name)) {
       const dropbox = new Dropbox({ accessToken: token$.value, fetch });
-      dropbox.filesGetThumbnail({path: props.file.path_lower, size: 'w32h32'})
-      .then((response) => {
-        let urlBlob = URL.createObjectURL(response.fileBlob);
-        updateUrl(urlBlob);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+      dropbox.filesGetThumbnail({ path: props.file.path_lower, size: 'w32h32' })
+        .then((response) => {
+          let urlBlob = URL.createObjectURL(response.fileBlob);
+          updateUrl(urlBlob);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
     }
   }, [props.file.name, props.file.path_lower])
 
-
-  if(fileType.test(props.file.name)){
+  // filetype: image
+  if (fileType.test(props.file.name)) {
     return (
       <img src={url} alt={props.file.name}></img>
     )
-  }
-  else if (props.file['.tag'] === 'file') {
+    // filetype: file
+  } else if (props.file['.tag'] === 'file') {
     return (
       <i class="material-icons">file_copy</i>
     )
-  }else{
+    // filetype: folder
+  } else if (props.file['.tag'] === 'folder') {
+    return (
+      <i class="material-icons">folder</i>
+    )
+  } else {
     return null
   }
 }
