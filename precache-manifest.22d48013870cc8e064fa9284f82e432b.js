3e7d94a862f996572ea382bb035e5c84af43d275
truptigaonkar <trups11.trupti@gmail.com>
2019-05-15 15:43:11 +0200

    Updates

---
+++ precache-manifest.22d48013870cc8e064fa9284f82e432b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4e9db573cf3e7ecac0965edcfd893c",
+    "url": "/mandatory-advanced-js5/index.html"
+  },
+  {
+    "revision": "2a38850aaf3a2ce61a10",
+    "url": "/mandatory-advanced-js5/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ecaa2ffa48005c2e17ad",
+    "url": "/mandatory-advanced-js5/static/css/main.2fc1d75c.chunk.css"
+  },
+  {
+    "revision": "2a38850aaf3a2ce61a10",
+    "url": "/mandatory-advanced-js5/static/js/2.f504314b.chunk.js"
+  },
+  {
+    "revision": "ecaa2ffa48005c2e17ad",
+    "url": "/mandatory-advanced-js5/static/js/main.c4015a9a.chunk.js"
+  },
+  {
+    "revision": "693ed353e3382e7179ea",
+    "url": "/mandatory-advanced-js5/static/js/runtime~main.d0f14806.js"
+  },
+  {
+    "revision": "7c908eb468dae9cc3f343a77ecb77060",
+    "url": "/mandatory-advanced-js5/static/media/logo.7c908eb4.png"
+  }
+]);
